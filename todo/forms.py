2fb81e7b78977a6bd5bfdc03f1d9cba2d7584b66
alokjais99 <alokjais99@gmail.com>
2019-10-23 18:48:01 +0530

    my first commit

---
+++ todo/forms.py
@@ -0,0 +1,6 @@
+from django import forms 
+
+class TodoForm(forms.Form):
+    text = forms.CharField(max_length=50, 
+        widget=forms.TextInput(
+            attrs={'class' : 'form-control', 'placeholder' : 'Enter todo Task', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
